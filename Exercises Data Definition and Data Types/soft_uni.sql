620b4499b204de0d43553896de2474a4f604566a
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-15 01:17:11 +0200

    add select name and set salary

--- Exercises Data Definition and Data Types/soft_uni.sql
+++ Exercises Data Definition and Data Types/soft_uni.sql
@@ -72,4 +72,21 @@ ORDER BY name ASC;
 SELECT * FROM employees 
 ORDER BY salary DESC;
 
+SELECT name 
+FROM towns
+ORDER BY name ASC;
 
+
+SELECT name 
+FROM departments
+ORDER BY name ASC;
+
+SELECT first_name, last_name, job_title, salary
+FROM employees
+ORDER BY salary DESC;
+
+UPDATE employees 
+SET salary = salary + (0.10 * salary);
+
+SELECT salary
+FROM employees;
