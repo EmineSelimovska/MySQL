86f6485f23b2fddc9748e74411a9364bbb8bb53d
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-16 00:07:38 +0200

    select email address

--- Basic CRUD - Exercise/employees.sql
+++ Basic CRUD - Exercise/employees.sql
@@ -3,3 +3,10 @@ SELECT * FROM soft_uni.employees;
 SELECT first_name, last_name,salary
 FROM employees
 ORDER BY employee_id;
+
+SELECT first_name, middle_name,last_name
+FROM employees
+ORDER BY employee_id;
+
+SELECT concat(first_name,'.',last_name,'@softuni.bg') as full_email_address
+FROM employees;
