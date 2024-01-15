1a95df6893d49f5332e6466371ad47652987f006
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-15 23:55:34 +0200

    add select name and employees

--- Basic CRUD - Exercise/departments.sql
+++ Basic CRUD - Exercise/departments.sql
@@ -2,3 +2,6 @@ SELECT * FROM soft_uni.departments;
 
 SELECT * FROM departments 
 ORDER BY department_id;
+
+SELECT name FROM departments 
+ORDER BY department_id;
---
+++ Basic CRUD - Exercise/employees.sql
@@ -0,0 +1,5 @@
+SELECT * FROM soft_uni.employees;
+
+SELECT first_name, last_name,salary
+FROM employees
+ORDER BY employee_id;
