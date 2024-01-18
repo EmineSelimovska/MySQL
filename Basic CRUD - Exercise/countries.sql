009627065047f9baef51bc78cbb2ea3d4842b835
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-18 21:58:11 +0200

    add task 22 and 23

--- Basic CRUD - Exercise/countries.sql
+++ Basic CRUD - Exercise/countries.sql
@@ -9,3 +9,9 @@ WHERE continent_code = 'EU'
 ORDER BY population DESC,
 country_name ASC
 LIMIT 30;
+
+SELECT country_name, country_code, IF(currency_code = 'EUR','Euro','Not Euro')
+ AS currency
+FROM 
+countries
+ORDER BY country_name;
