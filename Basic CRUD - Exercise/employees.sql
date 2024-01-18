bbe7d366ea561e8c83d783fb63eac8870d0ee889
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-18 17:28:29 +0200

    ADD TASK 19

--- Basic CRUD - Exercise/employees.sql
+++ Basic CRUD - Exercise/employees.sql
@@ -1,33 +1,8 @@
 SELECT * FROM soft_uni.employees;
 
-SELECT first_name, last_name,salary
-FROM employees
-ORDER BY employee_id;
+SELECT first_name, last_name, hire_date FROM 
+employees
+ORDER BY hire_date DESC
+limit 7;
 
-SELECT first_name, middle_name,last_name
-FROM employees
-ORDER BY employee_id;
-
-SELECT concat(first_name,'.',last_name,'@softuni.bg') as full_email_address
-FROM employees;
-
-SELECT DISTINCT salary FROM employees;
-
-SELECT *
-FROM employees 
-WHERE job_title = "Sales Representative"
-ORDER BY employee_id;
-
-SELECT first_name, last_name,job_title
-FROM employees
-WHERE salary between 20000 AND 30000
-ORDER BY employee_id;
-
-SELECT concat(first_name,' ',middle_name, ' ',last_name) as 'Full Name'
-FROM employees
-WHERE salary = 25000 || salary = 14000 || salary = 12500 || salary = 23600;
-
-SELECT first_name, last_name 
-FROM employees
-WHERE manager_id is Null;
 
