e48b8ecfeadc5e760f0fb60a7a8adb3041cf2332
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-18 17:57:50 +0200

    add peaks

--- Basic CRUD - Exercise/employees.sql
+++ Basic CRUD - Exercise/employees.sql
@@ -5,4 +5,15 @@ employees
 ORDER BY hire_date DESC
 limit 7;
 
+select department_id from departments
+where name in('Engineering', 'Tool Design','Marketing', 'Information Services');
+
+
+UPDATE employees 
+SET salary = salary * 1.12 
+WHERE department_id IN(1, 2, 4, 11);
+
+SELECT salary FROM employees;
+
+
 
