2208bc694474fd8e870f7dde25696569077829f9
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-19 15:31:47 +0200

    ADD TASK 03

--- Exercises Built-in Functions/employees.sql
+++ Exercises Built-in Functions/employees.sql
@@ -6,3 +6,14 @@ SELECT first_name, last_name
 FROM employees 
 WHERE first_name LIKE 'Sa%'
 ORDER BY employee_id;
+
+SELECT first_name, last_name 
+FROM employees 
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
+
+SELECT first_name
+FROM employees 
+WHERE (department_id = 3 or department_id = 10) AND
+YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
