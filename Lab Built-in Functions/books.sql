a21259ebe5ce88c02a897c7902a5ae718ce5f9d6
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-19 14:48:49 +0200

    add task 02 and 03

--- Lab Built-in Functions/books.sql
+++ Lab Built-in Functions/books.sql
@@ -1,5 +1,12 @@
 SELECT * FROM book_library.books;
 
 SELECT title FROM books
-WHERE title LIKE 'The%'
+WHERE title LIKE 'The%' 
 ORDER BY id;
+
+SELECT replace(title, 'The', '***')
+AS title FROM books
+WHERE substring(title, 1, 3) = 'The'
+ORDER BY id;
+
+SELECT round(SUM(cost),2) AS 'sum' FROM books ;
