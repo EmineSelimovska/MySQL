2a1c59511cb76c07e72132ec3a7927119f715b37
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-19 16:31:02 +0200

    add task 08 and 09

--- Exercises Built-in Functions/employees.sql
+++ Exercises Built-in Functions/employees.sql
@@ -1,25 +1,11 @@
 SELECT * FROM soft_uni.employees;
 
-use soft_uni;
+CREATE VIEW v_employees_hired_after_2000 as
+select first_name, last_name from employees
+where YEAR(hire_date) > 2000;
 
-SELECT first_name, last_name 
-FROM employees 
-WHERE first_name LIKE 'Sa%'
-ORDER BY employee_id;
+SELECT first_name, last_name FROM v_employees_hired_after_2000;
 
-SELECT first_name, last_name 
-FROM employees 
-WHERE last_name LIKE '%ei%'
-ORDER BY employee_id;
 
-SELECT first_name
-FROM employees 
-WHERE (department_id = 3 or department_id = 10) AND
-YEAR(hire_date) BETWEEN 1995 AND 2005
-ORDER BY employee_id;
-
-SELECT first_name, last_name 
-FROM employees 
-WHERE NOT (job_title LIKE '%engineer%')
-ORDER BY employee_id;
-
+SELECT first_name, last_name FROM employees
+WHERE length(last_name) = 5;
