1794c288ccfe4e49acf147186eff20bc21096d6f
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-19 15:37:45 +0200

    add task 04 with not like

--- Exercises Built-in Functions/employees.sql
+++ Exercises Built-in Functions/employees.sql
@@ -17,3 +17,8 @@ FROM employees
 WHERE (department_id = 3 or department_id = 10) AND
 YEAR(hire_date) BETWEEN 1995 AND 2005
 ORDER BY employee_id;
+
+SELECT first_name, last_name 
+FROM employees 
+WHERE NOT (job_title LIKE '%engineer%')
+ORDER BY employee_id;
