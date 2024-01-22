1f35e09a25f641e0a6556e9c907e91b052cb0d02
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-22 23:25:31 +0200

    add Deposits Sum

--- Exercises Data Aggregation/deposits.sql
+++ Exercises Data Aggregation/deposits.sql
@@ -16,3 +16,10 @@ SELECT deposit_group,
 	WHERE ( 
     SELECT MIN(magic_wand_size) from wizzard_deposits)
  LIMIT 1;
+ 
+ 
+ SELECT deposit_group,
+ SUM(deposit_amount) as 'total_sum'
+ FROM wizzard_deposits
+ GROUP BY deposit_group
+ ORDER BY total_sum;
