5f43f1d6cd3c82656d02f6315b49118f0258e954
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-22 23:51:46 +0200

    add Deposit Charge

--- Exercises Data Aggregation/deposits.sql
+++ Exercises Data Aggregation/deposits.sql
@@ -33,3 +33,18 @@ SELECT deposit_group,
  GROUP BY deposit_group
  ORDER BY deposit_group;
 
+ SELECT deposit_group,
+ SUM(deposit_amount) as 'total_sum'
+ FROM wizzard_deposits
+ WHERE magic_wand_creator = 'Ollivander family'
+ GROUP BY deposit_group
+ HAVING total_sum < 150000
+ ORDER BY total_sum DESC;
+ 
+ 
+ 
+ SELECT deposit_group, magic_wand_creator, MIN(deposit_charge) as min_deposit_charge
+ FROM wizzard_deposits
+ GROUP BY deposit_group,magic_wand_creator
+ ORDER BY magic_wand_creator,deposit_group;
+
