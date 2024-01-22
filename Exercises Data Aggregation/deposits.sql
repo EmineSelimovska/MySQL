4e0de173a4b75687c56bc9bf7f25308ac2f9da97
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-23 01:01:46 +0200

    add First Letter

--- Exercises Data Aggregation/deposits.sql
+++ Exercises Data Aggregation/deposits.sql
@@ -63,4 +63,14 @@ SELECT
    FROM wizzard_deposits
    GROUP BY age_group
    ORDER BY wizard_count;
+   
+   
+SELECT 
+    LEFT(first_name, 1) AS first_letter
+FROM
+    wizzard_deposits
+WHERE
+    deposit_group = 'Troll Chest'
+GROUP BY first_letter
+ORDER BY first_letter;
 
