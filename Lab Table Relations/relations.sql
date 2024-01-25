cab7ac99f17b02c456e8abab0a8931ec1fa31e69
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-25 17:16:15 +0200

    add  Mountains and Peaks

---
+++ Lab Table Relations/relations.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE relations;
+
+use relations;
+
+CREATE TABLE mountains(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE peaks(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+    mountain_id INT,
+    CONSTRAINT fk_peaks_mountains
+    FOREIGN KEY (mountain_id)  
+    REFERENCES mountains(id)
+)
+
+
+
