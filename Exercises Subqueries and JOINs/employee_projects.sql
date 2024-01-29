ba1a4e688d76b4151076de4c36288bd5ed405c6b
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-30 00:35:58 +0200

    add 5. Employees Without Project

---
+++ Exercises Subqueries and JOINs/employee_projects.sql
@@ -0,0 +1,8 @@
+SELECT * FROM soft_uni.projects;
+
+
+SELECT e.employee_id, e.first_name FROM employees e
+LEFT JOIN employees_projects ep on e.employee_id = ep.employee_id
+WHERE ep.project_id IS NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
