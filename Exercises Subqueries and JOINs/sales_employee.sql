2518d80dfbd1ab589ad6f99b58c241afd5ea0980
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-01-30 00:06:26 +0200

    Update sales_employee.sql

--- Exercises Subqueries and JOINs/sales_employee.sql
+++ Exercises Subqueries and JOINs/sales_employee.sql
@@ -1,7 +1,9 @@
 SELECT * FROM soft_uni.departments;
 
+
 SELECT e.employee_id, e.first_name, e.last_name, d.name as department_name
 FROM employees e
 JOIN departments d on e.department_id = d.department_id
 HAVING department_name = 'Sales'
 ORDER BY employee_id DESC;
+
