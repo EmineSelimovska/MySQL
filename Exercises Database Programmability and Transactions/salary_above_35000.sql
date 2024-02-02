155388281da77ab1b4473b00dfe0e6b2c8f77381
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-02 15:25:11 +0200

    add 1.1. Employees with Salary Above 35000

---
+++ Exercises Database Programmability and Transactions/salary_above_35000.sql
@@ -0,0 +1,15 @@
+SELECT * FROM soft_uni.employees;
+use soft_uni;
+
+
+DELIMITER $
+ CREATE PROCEDURE usp_get_employees_salary_above_35000()
+ BEGIN
+     SELECT first_name, last_name FROM employees 
+       WHERE salary > 35000
+       ORDER BY first_name, last_name, employee_id;
+ END $
+ 
+DELIMITER ;
+
+call usp_get_employees_salary_above_35000()
