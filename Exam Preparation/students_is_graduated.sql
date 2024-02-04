44822666a49c17faa3fbe1748cf1b6c2d1d6f986
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-04 23:00:28 +0200

    add 11. Graduate students

---
+++ Exam Preparation/students_is_graduated.sql
@@ -0,0 +1,16 @@
+SELECT * FROM universities_db.students;
+
+DELIMITER $
+CREATE PROCEDURE udp_graduate_all_students_by_year(year_started INT)
+BEGIN
+    UPDATE students s 
+    JOIN students_courses sc ON s.id = sc.student_id
+    JOIN courses c ON  sc.course_id = c.id 
+    SET s.is_graduated = 1
+    WHERE year(c.start_date) = year_started;
+END $
+
+DELIMITER ;
+
+CALL udp_graduate_all_students_by_year(2017);
+
