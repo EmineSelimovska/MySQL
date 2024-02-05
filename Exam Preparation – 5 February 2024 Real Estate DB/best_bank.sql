6a88bcf286e514af53d1079474bb646915764eed
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-06 01:13:38 +0200

    add 9. Size of the area

--- Exam Preparation – 5 February 2024 Real Estate DB/best_bank.sql
+++ Exam Preparation – 5 February 2024 Real Estate DB/best_bank.sql
@@ -5,3 +5,18 @@ FROM property_transactions
 GROUP BY bank_name
 HAVING count >= 9
 ORDER BY count DESC , bank_name ASC;
+
+
+SELECT address, area,
+case
+  when area <= 100 then 'small'
+  when area <= 200 then 'medium'
+  when area <= 500 then 'large'
+  when area > 500 then 'extra large'
+end as size
+from
+properties
+ORDER BY area ASC, address DESC;
+
+
+
