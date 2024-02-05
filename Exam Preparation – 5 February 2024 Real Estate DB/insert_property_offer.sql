cc55630bd7b3360533730ac3e8dd3b0647135086
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-06 00:36:54 +0200

    add offer by 2021

--- Exam Preparation – 5 February 2024 Real Estate DB/insert_property_offer.sql
+++ Exam Preparation – 5 February 2024 Real Estate DB/insert_property_offer.sql
@@ -27,3 +27,9 @@ phone, email, city_id FROM agents
 ORDER BY city_id DESC, phone DESC;
 
 
+SELECT property_id, agent_id, price, offer_datetime
+FROM property_offers 
+WHERE year(offer_datetime) = 2021
+ORDER BY price ASC 
+LIMIT 10;
+
