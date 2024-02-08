074f33660df0b8259869c330adc6012316115dfb
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-08 17:24:58 +0200

    add 7. Private flights

--- Retake Exam-Airlines DB/querying.sql
+++ Retake Exam-Airlines DB/querying.sql
@@ -10,3 +10,9 @@ SELECT flight_code, departure_country,
  WHERE year(departure) = 2022
  ORDER BY airplane_id, flight_code
  LIMIT 20;
+
+SELECT CONCAT(UPPER(substr(p.last_name ,1,2)), p.country_id) as flight_code,
+CONCAT(p.first_name,' ', p.last_name) as full_name,
+p.country_id from passengers p 
+WHERE p.id NOT IN (SELECT passenger_id from flights_passengers)
+ORDER BY p.country_id;
