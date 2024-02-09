12c5d1e70a1a4c4431608c4ecaa673385c8549b0
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-09 21:57:26 +0200

    add 11. Movie awards - Procedure

--- Database Basics MySQL Retake Exam - 10 April 2022/programmability.sql
+++ Database Basics MySQL Retake Exam - 10 April 2022/programmability.sql
@@ -27,3 +27,16 @@ DELIMITER ;
 SELECT udf_actor_history_movies_count('Stephan Lundberg')  AS 'history_movies';
 
 -- 11. Movie awards
+
+DELIMITER $ 
+CREATE PROCEDURE udp_award_movie(movie_title VARCHAR(50))
+BEGIN
+UPDATE actors a
+JOIN movies_actors ma ON a.id = ma.actor_id
+JOIN movies m ON m.id = ma.movie_id
+SET a.awards = a.awards + 1
+WHERE m.title = movie_title;
+END $
+DELIMITER ;
+
+CALL udp_award_movie('Tea For Two');
