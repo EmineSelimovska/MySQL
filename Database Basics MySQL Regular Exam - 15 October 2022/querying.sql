66699949295437731d3f3e8c6b2a09c089601d0a
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-09 14:04:21 +0200

    add 06 Birthdate

--- Database Basics MySQL Regular Exam - 15 October 2022/querying.sql
+++ Database Basics MySQL Regular Exam - 15 October 2022/querying.sql
@@ -5,3 +5,11 @@ SELECT * FROM restaurant_db.clients;
 SELECT id, first_name, last_name, 
 birthdate, card, review FROM clients
 ORDER BY birthdate DESC, id DESC;
+
+-- 06 Birthdate
+
+SELECT first_name, last_name, birthdate, review 
+FROM clients
+WHERE card IS NULL AND (YEAR(birthdate) BETWEEN 1978 AND 1993)
+ORDER BY last_name DESC, id ASC
+LIMIT 5;
